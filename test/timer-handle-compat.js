d99cb6a23f005c6274eff00da94c21406544a510
Robert Nagy <ronagy@icloud.com>
2026-07-10 21:30:40 +0200

    fix: support non-Node timer handles

---
+++ test/timer-handle-compat.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const net = require('node:net')
+const { afterEach, test } = require('node:test')
+
+const timers = require('../lib/util/timers')
+const connectH1 = require('../lib/dispatcher/client-h1')
+const {
+  kMaxHeadersSize,
+  kMaxResponseSize,
+  kParser
+} = require('../lib/core/symbols')
+
+const originalSetTimeout = globalThis.setTimeout
+const originalClearTimeout = globalThis.clearTimeout
+
+afterEach(() => {
+  timers.reset()
+  globalThis.setTimeout = originalSetTimeout
+  globalThis.clearTimeout = originalClearTimeout
+})
+
+function installNumericTimerHandles () {
+  let nextId = 0
+  const handles = new Map()
+
+  globalThis.setTimeout = (callback, delay, ...args) => {
+    const id = ++nextId
+    handles.set(id, originalSetTimeout(callback, delay, ...args))
+    return id
+  }
+
+  globalThis.clearTimeout = (id) => {
+    const handle = handles.get(id)
+    handles.delete(id)
+    return originalClearTimeout(handle ?? id)
+  }
+}
+
+test('fast timers support timer handles without refresh or unref', () => {
+  installNumericTimerHandles()
+
+  const first = timers.setFastTimeout(() => {}, 2_000)
+  const second = timers.setFastTimeout(() => {}, 2_000)
+
+  timers.tick()
+
+  timers.clearTimeout(first)
+  timers.clearTimeout(second)
+})
+
+test('the H1 parser supports timer handles without unref', async () => {
+  installNumericTimerHandles()
+
+  const socket = new net.Socket()
+  const client = {
+    [kMaxHeadersSize]: 16_384,
+    [kMaxResponseSize]: -1
+  }
+
+  await connectH1(client, socket)
+
+  try {
+    socket[kParser].setTimeout(10, 0)
+  } finally {
+    socket[kParser].destroy()
+    socket.removeAllListeners()
+    socket.destroy()
+  }
+})
